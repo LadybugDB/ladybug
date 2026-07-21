d38d4047091e3e0853824586afc3f3a9f4dec23f
Arun Sharma <arun@sharma-home.net>
2026-07-21 11:07:10 -0700

    Bump up version to 0.18.3

--- src/c_api/version.cpp
+++ src/c_api/version.cpp
@@ -6,7 +6,7 @@
 char* lbug_get_version() {
     auto version = lbug::main::Version::getVersion();
     if (version == nullptr || version[0] == '\0') {
-        version = "0.18.2";
+        version = "0.18.3";
     }
     return convertToOwnedCString(version);
 }
